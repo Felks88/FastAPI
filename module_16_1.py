e943c429e881e83249066f288e215835bf30a326
Felks88 <sanix2t@mail.ru>
2025-01-15 00:44:55 +1000

    Add files via upload

--- module_16_1.py
+++ module_16_1.py
@@ -4,17 +4,17 @@ app = FastAPI()
 
 
 @app.get("/")
-async def welcome():
+async def welcome() -> str:
     return "Главная страница"
 
 
 @app.get("/user/admin")
-async def welcome_admin():
+async def welcome_admin() -> str:
     return "Вы вошли как администратор"
 
 
 @app.get("/user/{user_id}")
-async def get_user(user_id: int):
+async def get_user(user_id: int) -> str:
     return f"Вы вошли как пользователь № {user_id}"
 
 
