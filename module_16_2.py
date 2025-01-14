65787e87e8fc26a7c748b7f9b5774f0c40572e30
Felks88 <sanix2t@mail.ru>
2025-01-15 08:18:09 +1000

    Add files via upload

--- module_16_2.py
+++ module_16_2.py
@@ -23,5 +23,5 @@ async def get_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter
 @app.get("/user/{username}/{age}")
 async def userinfo(
         username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")],
-        age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> dict:
-    return {"Информация о пользователе. Имя": username, "Возраст": age}
+        age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
